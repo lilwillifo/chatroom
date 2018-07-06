c43296bd32ab519e0446714875db64e6e2c23afd
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-06 10:25:05 -0600

    add messages

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,30 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      flash[:success] = 'Chatroom added!'
+      redirect_to rooms_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    # includes used for eager loading
+    @room = Room.includes(:messages).find_by(id: params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title)
+  end
+end
